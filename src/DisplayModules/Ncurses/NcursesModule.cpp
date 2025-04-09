6f913c11401cdf33c29c4cfbc871df2bf64f3832
Edgar Brunet <edgar.brunet@epitech.eu>
2025-04-09 14:54:30 +0200

    FEAT(src/DisplayModules/Ncurses/NcursesModule.cpp): adding the management of the color based on the getProperties methods

--- src/DisplayModules/Ncurses/NcursesModule.cpp
+++ src/DisplayModules/Ncurses/NcursesModule.cpp
@@ -9,6 +9,23 @@
 
 #include <iostream>
 #include <fstream>
+#include <cmath>
+
+short getNearestColor(u_int32_t color) {
+    unsigned char r = GET_RED(color);
+    unsigned char g = GET_GREEN(color);
+    unsigned char b = GET_BLUE(color);
+
+    if (r > 200 && g > 200 && b > 200) return COLOR_WHITE;
+    if (r < 50 && g < 50 && b < 50) return COLOR_BLACK;
+    if (r > 200 && g < 100 && b < 100) return COLOR_RED;
+    if (r < 100 && g > 200 && b < 100) return COLOR_GREEN;
+    if (r < 100 && g < 100 && b > 200) return COLOR_BLUE;
+    if (r > 200 && g > 200 && b < 100) return COLOR_YELLOW;
+    if (r > 200 && g < 100 && b > 200) return COLOR_MAGENTA;
+    if (r < 100 && g > 200 && b > 200) return COLOR_CYAN;
+    return COLOR_WHITE;
+}
 
 NcursesModule::NcursesModule() : _window(nullptr), _isOpen(false),
                                  _gameWidth(800), _gameHeight(600) {}
@@ -63,6 +80,17 @@ void NcursesModule::openWindow() {
     getmaxyx(stdscr, _termHeight, _termWidth);
     _window = stdscr;
     _isOpen = true;
+
+    if (has_colors()) {
+        start_color();
+        init_pair(1, COLOR_WHITE, COLOR_BLACK);
+        init_pair(2, COLOR_RED, COLOR_BLACK);
+        init_pair(3, COLOR_GREEN, COLOR_BLACK);
+        init_pair(4, COLOR_BLUE, COLOR_BLACK);
+        init_pair(5, COLOR_YELLOW, COLOR_BLACK);
+        init_pair(6, COLOR_MAGENTA, COLOR_BLACK);
+        init_pair(7, COLOR_CYAN, COLOR_BLACK);
+    }
     clear();
     refresh();
 }
@@ -93,6 +121,9 @@ void NcursesModule::display(std::map<std::string, std::unique_ptr<IObject>>& obj
         if (object) {
             std::pair<int, int> pos = object->getPosition();
             auto [termX, termY] = scaleCoords(pos.first, pos.second);
+
+            short colorPair = 1;
+
             if (object->getType() == SPRITE) {
                 char displayChar = '#';
                 auto spriteValue = object->getSprite();
@@ -102,13 +133,47 @@ void NcursesModule::display(std::map<std::string, std::unique_ptr<IObject>>& obj
                         displayChar = *spritePtr;
                     }
                 }
+                IObject::SpriteProperties props = std::get<IObject::SpriteProperties>(object->getProperties());
+                if (has_colors()) {
+                    short colorNum = getNearestColor(props.textColor);
+                    switch (colorNum) {
+                    case COLOR_RED: colorPair = 2; break;
+                    case COLOR_GREEN: colorPair = 3; break;
+                    case COLOR_BLUE: colorPair = 4; break;
+                    case COLOR_YELLOW: colorPair = 5; break;
+                    case COLOR_MAGENTA: colorPair = 6; break;
+                    case COLOR_CYAN: colorPair = 7; break;
+                    default: colorPair = 1; break;
+                    }
+                    attron(COLOR_PAIR(colorPair));
+                }
                 mvaddch(termY, termX, displayChar);
+                if (has_colors()) {
+                    attroff(COLOR_PAIR(colorPair));
+                }
                 objectsDisplayed++;
             }
             else if (object->getType() == TEXT) {
-                auto props = std::get<IObject::TextProperties>(object->getProperties());
+                IObject::TextProperties props = std::get<IObject::TextProperties>(object->getProperties());
                 std::string displayText = props.text;
+
+                if (has_colors()) {
+                    short colorNum = getNearestColor(props.color);
+                    switch (colorNum) {
+                    case COLOR_RED: colorPair = 2; break;
+                    case COLOR_GREEN: colorPair = 3; break;
+                    case COLOR_BLUE: colorPair = 4; break;
+                    case COLOR_YELLOW: colorPair = 5; break;
+                    case COLOR_MAGENTA: colorPair = 6; break;
+                    case COLOR_CYAN: colorPair = 7; break;
+                    default: colorPair = 1; break;
+                    }
+                    attron(COLOR_PAIR(colorPair));
+                }
                 mvprintw(termY, termX, "%s", displayText.c_str());
+                if (has_colors()) {
+                    attroff(COLOR_PAIR(colorPair));
+                }
                 objectsDisplayed++;
             }
         }
